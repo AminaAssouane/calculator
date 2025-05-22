ad8d611bb7176153123938929d9111a33b7eaa6c
Amina <minassouane@gmail.com>
2025-05-22 19:07:47 +0100

    Implement first half of calculator project

--- script.js
+++ script.js
@@ -14,25 +14,51 @@ function divide(a, b) {
   return a / b;
 }
 
-let num1;
-let op;
-let num2;
-
 function operate(a, op, b) {
   switch (op) {
     case "+":
-      add(a, b);
+      return add(a, b);
       break;
     case "-":
-      substract(a, b);
+      return substract(a, b);
       break;
     case "*":
-      multiply(a, b);
+      return multiply(a, b);
       break;
     case "/":
-      divide(a, b);
+      return divide(a, b);
       break;
     default:
       console.log("Not an operand.");
   }
 }
+const display = document.querySelector("#display");
+const numButton = document.querySelectorAll(".number");
+let number = null;
+let operator = null;
+let number2 = null;
+
+numButton.forEach((button) =>
+  button.addEventListener("click", () => {
+    display.textContent = display.textContent.concat(button.textContent);
+    if (operator === null) number = Number(display.textContent);
+    else number2 = Number(display.textContent);
+  })
+);
+
+const opButton = document.querySelectorAll(".operator");
+
+opButton.forEach((button) =>
+  button.addEventListener("click", () => {
+    operator = button.textContent;
+    display.textContent = "";
+  })
+);
+
+const equal = document.querySelector("#\\=");
+equal.addEventListener("click", () => {
+  display.textContent = operate(number, operator, number2).toString();
+  number = Number(display.textContent);
+  operator = null;
+  number2 = null;
+});
